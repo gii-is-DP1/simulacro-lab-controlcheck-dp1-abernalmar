6f35c5e5e58f64bd3badc396bc1f39f0df4d2859
abernalmar <abernalmar@gmail.com>
2022-10-30 12:18:20 +0100

    Hechos apartados 1-8

--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,21 +2,44 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+	
+	//Ej5
+	@Autowired
+	ProductRepository productRepo;
+	
     public List<Product> getAllProducts(){
-        return null;
+    	List<Product> p = productRepo.findAll();
+        return p;
     }
+    //
+    
 
-    public List<Product> getProductsCheaperThan(double price) {
-        return null;
-    }
-
+    //Ej6
     public ProductType getProductType(String typeName) {
-        return null;
+    	ProductType tp = productRepo.findProductType(typeName);
+        return tp;
     }
+    //
+    
+    //Ej8
+    public List<Product> getProductsCheaperThan(double price){
+    	List<Product> p = this.productRepo.findByPriceLessThan(price);
+    	return p;
+    }
+    //
 
     public Product save(Product p){
         return null;       
+    }
+    
+    public List<ProductType> findAllProductTypes(){
+    	List<ProductType> p = productRepo.findAllProductTypes();
+    	return p;
     }
 
     
